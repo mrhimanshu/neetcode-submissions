c3df333347331790a25246795733cea60673c30c
Himanshu Saini <hsaini102@gmail.com>
2026-08-17 23:11:42 +0100

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 1.0
+print(type(variable))
+variable = False
+print(type(variable))
+variable = 'Him'
+print(type(variable))
+variable = [1,2]
+print(type(variable))
