9c635b8572f3187baf3497870270b2074e63f858
Himanshu Saini <hsaini102@gmail.com>
2026-08-17 23:10:16 +0100

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,10 @@
+integer_type = 1
+float_type = 1.0
+bool_type = False
+string_type = 'Him'
+list_type = [2, 3, 4.0]
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
